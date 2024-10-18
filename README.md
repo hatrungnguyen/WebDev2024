0bcadc2b2cf4db2907d4669ab6a45da7ae2894d5
hoangviet <kinkinkin229@gmail.com>
2024-10-18 16:15:11 +0700

    Header + Home + ReadMe + Logo picture

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ USTH ICT 2024 Web Application Development
 
 Student Info
 =======================
-
-* Name: Nguyen Ha Trung
-* ID: 22BI13436
 * Group ID: 22
 * Project Name: Movie Review Platform
+* Name and student ID: 
+  * Nguyễn Hà Trung - 22BI13436
+  * Đào Quý Tùng - 22BI13451
+  * Lê Quang Tùng - 22Bi13454
+  * Đỗ Lê Hoàng Việt - 22BI13463
+  * Nguyễn Tri Anh Vũ - 
+
+
+
---
+++ src/components/home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = ({ movies }) => {
+    return (
+        <div className="home">
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <div key={movie.id} className="movie-card">
+                        <h2>{movie.title}</h2>
+                        <img src={movie.image} alt={movie.title} className="movie-image" />
+
+                        <Link to={`/Trailer/${movie.trailerUrl}`} className="trailer-button">
+                            Watch Trailer
+                        </Link>
+                        <Link to={`/Reviews/${movie.id}`} className="review-button">
+                            Review
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
