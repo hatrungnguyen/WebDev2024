0c2a578e499a4975f54fb86cf59057fd566acd12
hoangviet <kinkinkin229@gmail.com>
2024-10-18 16:21:34 +0700

    Header + Home + ReadMe + Logo picture

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import {NavLink} from "react-router-dom";
-import logo from './page_logo.jpg'
+import logo from './page_logo.png'
 
 const Header = () => {
 
