3c8f105ec99bfe972a5cba0c25ebe16effddf9d6
QuyTungDao <tbbuong@gmail.com>
2024-10-18 19:37:09 +0700

    Design review's page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom' ;
 const Home = ({ movies }) => {
     return (
         <div className="home">
-            <div className="movies-grid">
+            <div className="movies-grid" style={{ backgroundColor: '#29292e' }}>
                 {movies.map((movie) => (
-                    <div key={movie.id} className="movie-card">
+                    <div key={movie.id} className="movie-card" >
                         <h2>{movie.title}</h2>
                         <img src={movie.image} alt={movie.title} className="movie-image" />
 
